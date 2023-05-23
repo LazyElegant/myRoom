9dd81653553963fc0660c242a3c28d6b61cbc8d8
LazyElegant <59470906+LazyElegant@users.noreply.github.com>
2023-05-23 19:01:13 +0800

    Delete spring boot directory

--- spring boot/视频网/src/main/resources/static/js/common.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('.header-search-trigger').click(function(){
-		if($('.header-search-wrapper').css('display')=='block'){
-			$('.header-search-wrapper').fadeToggle();
-		}else{	
-			$('.header-search-wrapper').fadeTo('slow',1);
-		}
-	})
-})
