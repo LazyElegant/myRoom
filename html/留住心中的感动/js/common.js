7a1df07f293676d075c51344d45c57ec7a64dae7
LazyElegant <59470906+LazyElegant@users.noreply.github.com>
2023-05-30 17:33:08 +0800

    Delete html directory

--- html/留住心中的感动/js/common.js
+++
@@ -1,7 +0,0 @@
-let count = 2
-$(function(){ 
-	setInterval(function(){
-		$(".rslides").css("background-image","url(img/slider"+ count+ ".jpg)");
-		count = count >= 3 ? 1 : count + 1;
-	},3000);
-})
